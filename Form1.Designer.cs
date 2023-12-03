eef2d168ceaeb4db74b13810685a4db1a39957a6
Hazel Boyaalıcı <107104140+Hazelboyaalici@users.noreply.github.com>
2023-12-03 22:42:20 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.teknolojikAletlerDBDataSet = new WindowsFormsApp1.TeknolojikAletlerDBDataSet();
+            this.telefonBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.telefonTableAdapter = new WindowsFormsApp1.TeknolojikAletlerDBDataSetTableAdapters.telefonTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.teknolojikAletlerDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.telefonBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // teknolojikAletlerDBDataSet
+            // 
+            this.teknolojikAletlerDBDataSet.DataSetName = "TeknolojikAletlerDBDataSet";
+            this.teknolojikAletlerDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // telefonBindingSource
+            // 
+            this.telefonBindingSource.DataMember = "telefon";
+            this.telefonBindingSource.DataSource = this.teknolojikAletlerDBDataSet;
+            // 
+            // telefonTableAdapter
+            // 
+            this.telefonTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(843, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Depoda olan tüm malları listele";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(843, 352);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 398);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.teknolojikAletlerDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.telefonBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private TeknolojikAletlerDBDataSet teknolojikAletlerDBDataSet;
+        private System.Windows.Forms.BindingSource telefonBindingSource;
+        private TeknolojikAletlerDBDataSetTableAdapters.telefonTableAdapter telefonTableAdapter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TeknolojikAletlerDB;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'teknolojikAletlerDBDataSet.telefon' table. You can move, or remove it, as needed.
+            this.telefonTableAdapter.Fill(this.teknolojikAletlerDBDataSet.telefon);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut=new SqlCommand();
+            komut.CommandText = "SELECT * FROM telefon";
+            komut.Connection = baglanti;
+
+            SqlDataAdapter adap = new SqlDataAdapter(komut);
+            DataTable tablo = new DataTable();
+
+            adap.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+
+
+                }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
